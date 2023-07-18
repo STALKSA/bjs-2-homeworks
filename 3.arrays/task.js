43500498e2d786baf1c2849a40addc3aeca680ce
ksenia <ksy184694@gmail.com>
2023-07-18 20:24:44 +0300

    adding functions

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    return arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filterUsers = users.filter(user => user.gender === gender);
+
+    if (filterUsers.length === 0) {
+        return 0;
+    }
+
+    const totalAge = filterUsers.reduce((sum, user) => sum + user.age, 0);
+    const averageAge = totalAge / filterUsers.length;
+
+    return averageAge;
 }
